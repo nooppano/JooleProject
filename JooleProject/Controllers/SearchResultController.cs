3c4fe43b6dbc2d849d81101ee78d488ce236c965
Josh <shane.oh.work@gmail.com>
2022-02-23 15:21:17 -0500

    SearchResult page works with searching in navbar.

--- JooleProject/Controllers/SearchResultController.cs
+++ JooleProject/Controllers/SearchResultController.cs
@@ -1,17 +1,41 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
-
+using JooleProject.Models;
 namespace JooleProject.Controllers
 {
     public class SearchResultController : Controller
     {
+        private ItlizeJooleDBEntities1 db = new ItlizeJooleDBEntities1();
+        
         // GET: SearchResult
-        public ActionResult SearchResult()
+        public ActionResult SearchResult(String CatIDSearch, String SearchSubcat)
         {
-            return View();
+            ViewBag.catselect = new SelectList(db.tblCategories, "CatID", "CategoryName");
+            ViewBag.CatIDSearch = CatIDSearch;
+            ViewBag.SearchSubcat = SearchSubcat;
+
+            /*var products = from p in db.tblProducts
+                           select p;*/
+
+            /*var test = from s in db.tblProducts join sa in db.tblSubCategories on s.SubCatID equals sa.SubCatID where sa.SubCatName==(SearchSubcat) select s;*/
+
+            var viewmodelResult = from p in db.tblProducts
+                                  join k in db.tblSubCategories on p.SubCatID equals k.SubCatID
+                                  select new SearchResultViewModel { product = p, subCategory = k };
+
+            if (!String.IsNullOrEmpty(CatIDSearch) || !String.IsNullOrEmpty(SearchSubcat))
+            {
+
+                viewmodelResult = viewmodelResult.Where(p => p.product.CatID.ToString().Contains(CatIDSearch) && p.subCategory.SubCatName.Contains(SearchSubcat));
+            }
+
+            return View(viewmodelResult.ToList());
         }
     }
 }
