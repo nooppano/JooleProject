885736f6e70a283b2fb112dfca4bb916a7284c7a
BirkanAk <bak1@binghamton.edu>
2022-02-22 23:32:18 -0500

    ViewModel created. Join is succesful. TODO: Filter the string

---
+++ JooleProject/Models/SearchResultViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace JooleProject.Models
+{
+    public class SearchResultViewModel
+    {
+        public tblProduct product { get; set; }
+        public tblSubCategory subCategory { get; set; }
+    }
+}
