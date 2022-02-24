93c51fab694e2e2b60f6291ddee7c24bff46a933
BirkanAk <bak1@binghamton.edu>
2022-02-23 23:14:37 -0500

    Search redirecting to another page complete! TODO: css

--- JooleProject/Controllers/SearchController.cs
+++ JooleProject/Controllers/SearchController.cs
@@ -12,13 +12,26 @@ namespace JooleProject.Controllers
 {
     public class SearchController : Controller
     {
-
         private ItlizeJooleDBEntities1 db = new ItlizeJooleDBEntities1();
 
-
-        public ActionResult Search(string searchstr)
+        public ActionResult Search(String CatIDSearch, String SearchSubcat)
         {
-            return View();
+            var viewmodelResult = from p in db.tblProducts join k in db.tblSubCategories on p.SubCatID equals k.SubCatID select new SearchResultViewModel { product = p, subCategory = k };
+
+            if (!String.IsNullOrEmpty(CatIDSearch) && !String.IsNullOrEmpty(SearchSubcat))
+            {
+
+                viewmodelResult = viewmodelResult.Where(p => p.product.CatID.ToString().Contains(CatIDSearch.ToString()) && p.subCategory.SubCatName.ToLower().Contains(SearchSubcat.ToLower()));
+            }
+            else if (!String.IsNullOrEmpty(CatIDSearch))
+            {
+                viewmodelResult = viewmodelResult.Where(p => p.product.CatID.ToString().Contains(CatIDSearch.ToString()));
+            }
+            else if (!String.IsNullOrEmpty(SearchSubcat))
+            {
+                viewmodelResult = viewmodelResult.Where(p => p.subCategory.SubCatName.ToLower().Contains(SearchSubcat.ToLower()));
+            }
+            return View(viewmodelResult.ToList());
         }
 
         // GET: Search
@@ -34,26 +47,7 @@ namespace JooleProject.Controllers
 
             /*var test = from s in db.tblProducts join sa in db.tblSubCategories on s.SubCatID equals sa.SubCatID where sa.SubCatName==(SearchSubcat) select s;*/
 
-            var viewmodelResult = from p in db.tblProducts
-                                  join k in db.tblSubCategories on p.SubCatID equals k.SubCatID
-                                  select new SearchResultViewModel { product = p, subCategory = k };
-
-            if (!String.IsNullOrEmpty(CatIDSearch) && !String.IsNullOrEmpty(SearchSubcat))
-            {
-
-                viewmodelResult = viewmodelResult.Where(p => p.product.CatID.ToString().Contains(CatIDSearch.ToString()) && p.subCategory.SubCatName.ToLower().Contains(SearchSubcat.ToLower()));
-            }
-            else if (!String.IsNullOrEmpty(CatIDSearch))
-            {
-                viewmodelResult = viewmodelResult.Where(p => p.product.CatID.ToString().Contains(CatIDSearch.ToString()));
-            }
-            else if (!String.IsNullOrEmpty(SearchSubcat))
-            {
-                viewmodelResult = viewmodelResult.Where(p => p.subCategory.SubCatName.ToLower().Contains(SearchSubcat.ToLower()));
-            }
-
-
-            return View(viewmodelResult.ToList());
+            return View();
         }
 
         // GET: Search/Details/5 
