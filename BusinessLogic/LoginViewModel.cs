fd8b7fd4de2fe8505cfde70539e68d193a07ec0b
Ronsard Malonda <ronsardmalonda@live.com>
2022-03-01 01:06:40 -0500

    aaaaaaaaaaaaaaaa

--- BusinessLogic/LoginViewModel.cs
+++ BusinessLogic/LoginViewModel.cs
@@ -8,7 +8,7 @@ namespace BusinessLogic
 {
     public class LoginViewModel
     {
-        public string Username { get; set; }
+        public string Username { get; set;}
         public string Password { get; set; }
     }
 }
