9d46b1b3b90a188c8df0aa7ab91bec19c99d0b77
BirkanAk <bak1@binghamton.edu>
2022-02-28 22:42:23 -0500

    testing ViewModel

---
+++ DataAccessLayer/SearchResultDAL.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DataAccessLayer
+{
+
+    public class SearchResultDAL
+    {
+
+        public ProductDAL product { get; set; }
+        public SubcatDAL subCategory { get; set; }
+
+        public IQueryable test()
+        {
+            SearchResultDAL srd = new SearchResultDAL();
+
+            var viewmodelResult = from p in srd.product
+                                      join k in srd.subCategory on p.SubCatID equals k.SubCatID
+                                      select new SearchResultViewModel { product = p, subCategory = k };
+        }
+    
+}
