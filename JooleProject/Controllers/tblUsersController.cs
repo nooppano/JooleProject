0d13316338b781e75f6456ce140abef96e6fee57
Ronsard Malonda <ronsardmalonda@live.com>
2022-02-28 23:37:50 -0500

    aaaaaa

--- JooleProject/Controllers/tblUsersController.cs
+++ JooleProject/Controllers/tblUsersController.cs
@@ -6,122 +6,122 @@ using System.Linq;
 using System.Net;
 using System.Web;
 using System.Web.Mvc;
-using JooleProject.Models;
+//using JooleProject.Models;
 
 namespace JooleProject.Controllers
 {
     public class tblUsersController : Controller
     {
-        private ItlizeJooleDBEntities1 db = new ItlizeJooleDBEntities1();
+    //    private ItlizeJooleDBEntities1 db = new ItlizeJooleDBEntities1();
 
-        // GET: tblUsers
-        public ActionResult Index()
-        {
-            return View(db.tblUsers.ToList());
-        }
+    //    // GET: tblUsers
+    //    public ActionResult Index()
+    //    {
+    //        return View(db.tblUsers.ToList());
+    //    }
 
-        // GET: tblUsers/Details/5
-        public ActionResult Details(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            tblUser tblUser = db.tblUsers.Find(id);
-            if (tblUser == null)
-            {
-                return HttpNotFound();
-            }
-            return View(tblUser);
-        }
+    //    // GET: tblUsers/Details/5
+    //    public ActionResult Details(int? id)
+    //    {
+    //        if (id == null)
+    //        {
+    //            return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+    //        }
+    //        tblUser tblUser = db.tblUsers.Find(id);
+    //        if (tblUser == null)
+    //        {
+    //            return HttpNotFound();
+    //        }
+    //        return View(tblUser);
+    //    }
 
-        // GET: tblUsers/Create
-        public ActionResult Create()
-        {
-            return View();
-        }
+    //    // GET: tblUsers/Create
+    //    public ActionResult Create()
+    //    {
+    //        return View();
+    //    }
 
-        // POST: tblUsers/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "UserID,Username,Password,Email,Photo")] tblUser tblUser)
-        {
-            if (ModelState.IsValid)
-            {
-                db.tblUsers.Add(tblUser);
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
+    //    // POST: tblUsers/Create
+    //    // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+    //    // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+    //    [HttpPost]
+    //    [ValidateAntiForgeryToken]
+    //    public ActionResult Create([Bind(Include = "UserID,Username,Password,Email,Photo")] tblUser tblUser)
+    //    {
+    //        if (ModelState.IsValid)
+    //        {
+    //            db.tblUsers.Add(tblUser);
+    //            db.SaveChanges();
+    //            return RedirectToAction("Index");
+    //        }
 
-            return View(tblUser);
-        }
+    //        return View(tblUser);
+    //    }
 
-        // GET: tblUsers/Edit/5
-        public ActionResult Edit(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            tblUser tblUser = db.tblUsers.Find(id);
-            if (tblUser == null)
-            {
-                return HttpNotFound();
-            }
-            return View(tblUser);
-        }
+    //    // GET: tblUsers/Edit/5
+    //    public ActionResult Edit(int? id)
+    //    {
+    //        if (id == null)
+    //        {
+    //            return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+    //        }
+    //        tblUser tblUser = db.tblUsers.Find(id);
+    //        if (tblUser == null)
+    //        {
+    //            return HttpNotFound();
+    //        }
+    //        return View(tblUser);
+    //    }
 
-        // POST: tblUsers/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "UserID,Username,Password,Email,Photo")] tblUser tblUser)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Entry(tblUser).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-            return View(tblUser);
-        }
+    //    // POST: tblUsers/Edit/5
+    //    // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+    //    // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+    //    [HttpPost]
+    //    [ValidateAntiForgeryToken]
+    //    public ActionResult Edit([Bind(Include = "UserID,Username,Password,Email,Photo")] tblUser tblUser)
+    //    {
+    //        if (ModelState.IsValid)
+    //        {
+    //            db.Entry(tblUser).State = EntityState.Modified;
+    //            db.SaveChanges();
+    //            return RedirectToAction("Index");
+    //        }
+    //        return View(tblUser);
+    //    }
 
-        // GET: tblUsers/Delete/5
-        public ActionResult Delete(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            tblUser tblUser = db.tblUsers.Find(id);
-            if (tblUser == null)
-            {
-                return HttpNotFound();
-            }
-            return View(tblUser);
-        }
+    //    // GET: tblUsers/Delete/5
+    //    public ActionResult Delete(int? id)
+    //    {
+    //        if (id == null)
+    //        {
+    //            return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+    //        }
+    //        tblUser tblUser = db.tblUsers.Find(id);
+    //        if (tblUser == null)
+    //        {
+    //            return HttpNotFound();
+    //        }
+    //        return View(tblUser);
+    //    }
 
-        // POST: tblUsers/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(int id)
-        {
-            tblUser tblUser = db.tblUsers.Find(id);
-            db.tblUsers.Remove(tblUser);
-            db.SaveChanges();
-            return RedirectToAction("Index");
-        }
+    //    // POST: tblUsers/Delete/5
+    //    [HttpPost, ActionName("Delete")]
+    //    [ValidateAntiForgeryToken]
+    //    public ActionResult DeleteConfirmed(int id)
+    //    {
+    //        tblUser tblUser = db.tblUsers.Find(id);
+    //        db.tblUsers.Remove(tblUser);
+    //        db.SaveChanges();
+    //        return RedirectToAction("Index");
+    //    }
 
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing)
-            {
-                db.Dispose();
-            }
-            base.Dispose(disposing);
-        }
+    //    protected override void Dispose(bool disposing)
+    //    {
+    //        if (disposing)
+    //        {
+    //            db.Dispose();
+    //        }
+    //        base.Dispose(disposing);
+    //    }
     }
 }
